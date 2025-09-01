374bc210a20cae3b0611e23ea806614db7441fcd
Wael Al-Mallah <93313945+wael003@users.noreply.github.com>
2025-09-01 20:23:42 +0300

    Update seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('../server/models/User'); 
+const User = require('../server/models/Users'); 
 
 const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/TaskTracker';
 
